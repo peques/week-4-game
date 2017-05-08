53e16263edd4c659568031874bc3e3f632e94b71
Saul Perea-Garayzar <saulpg@Sauls-MacBook-Air.local>
2017-05-08 09:53:06 -0700

    2nd Upload

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,33 +2,57 @@ var randomNumberArray = [21, 32, 43, 54, 65, 76, 87, 98, 109];
 var houseValues = [3, 5, 7, 10];
 var playerScore = 0;
 var wins = 0;
-var losses =0;
+var losses = 0;
 var magicNumber = 0;
-
 
 
 //Selects a random number from the available choices in randomNumberArray
 var magicNumber = randomNumberArray[Math.floor(Math.random() * randomNumberArray.length)];
+  $("#number").html("Magic Number: " + magicNumber);
+  console.log(magicNumber);
+
 
 //Function updating playerScore
 var updatePlayerScore = function() {
-  
-  document.querySelector('#score').innerHTML = "Your Score: " + playerScore;
+  document.getElementById("game").innerHTML = "playerScore"
 };
+
+
+$('button').click(function(){
+  playerScore = Number(playerScore) + Number($(this).val());
+   $('#score').text("Score: "+ playerScore);
+    
+    if (playerScore === magicNumber){
+                wins++;
+                $("#wins").html("Wins: " + wins);
+                alert("Yes! You Are Indeed a Powerful Witch/Wizard.");
+                reset();
+            } else if (playerScore > magicNumber){ 
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            // document.getElementById('#losses').innerHTML = "Losses: " + losses;
+            //   console.log(document.getElementById('#losses').innerHTML);
+            alert("The Dementors Are Coming to Take You to Azkaban!");
+            // Then we'll call the reset. 
+            reset();
+            console.log(playerScore);
+        }
+ });
+
+
 
 //Function giving House crests value
 function houseValue() {
 
-    for (var i = 1; i < 5; i++) {
+    for (var i = 0; i < 4; i++) {
         $('.btn-' + i).val(getRandom(houseValues));
+        console.log(houseValues);
     }
     houseValue();
-}
+};
 
 
-$('button').click(function(){
-  playerScore = Number(theTotal) + Number($(this).val());
-   $('#score').text("Score: "+ playerScore);
+
 
 
 
@@ -40,17 +64,7 @@ var reset = function() {
   losses = 0;
   playerScore = 0;
 
+  $("#score").html("Your Score: " + playerScore);
 
-    if (playerScore === magicNumber){
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("Yes! You Are Indeed a Powerful Witch/Wizard.");
-                reset();
-            } else if(playerScore > magicNumber){ 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("The Dementors Are Coming to Take You to Azkaban!");
-            // Then we'll call the reset. 
-            reset();
-        }
       };
+      
